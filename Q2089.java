d89f4f1a1864ba9f5f26a02bf012d05589e019b9
Zhang Yaoxin <80233644+XuanJi0208@users.noreply.github.com>
2022-06-08 11:28:58 +0800

    Update Q2089.java
    。

--- Q2089.java
+++ Q2089.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Q2089 {//很遗憾这是一篇错误答案，主要原因可能是sort方法调用失败
                     //这个代码中，把Sort方法直接改为Arrays.sort()方法即可直接出答案
     public List<Integer> targetIndices(int[] nums, int target) {
-        //代码主要分为两个部分，第一部分是排序，考虑冒泡和快排
+        //代码主要分为两个部分，第一部分是排序
         //第二个部分是搜索目标下标，在排序后查找比较容易。
         int l = nums.length;
         List<Integer> i = new ArrayList<>();
@@ -25,8 +25,8 @@ public class Q2089 {//很遗憾这是一篇错误答案，主要原因可能是s
             a = b;
             b = temp;
             return;
-        }//写个交换函数给冒泡和快排用
-    public void Sort(int[]nums, int l){//先写个冒泡玩一下
+        }//写个交换函数给排序用
+    public void Sort(int[]nums, int l){//先写个冒泡试一下
         boolean flag = false;
         for(int i = 0;i < l-1;i++){
             for(int j = l-1; j > i;j--){
