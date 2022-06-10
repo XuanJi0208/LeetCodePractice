b1ba43e06435872cf65d67b05d66d4cca17042ab
yaoxin <yaoxin.zhang@ucdconnect.ie>
2022-06-10 18:48:35 +0800

    Q1365 - 有多少小于当前数字的数字

--- Q1365.java
+++ Q1365.java
@@ -1,4 +1,9 @@
 public class Q1365 {
+
+    //给你一个数组nums，对于其中每个元素nums[i]，请你统计数组中比它小的所有数字的数目。
+    //换而言之，对于每个nums[i]你必须计算出有效的j的数量，其中 j 满足j != i 且 nums[j] < nums[i]。
+    //以数组形式返回答案。
+
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int l = nums.length;
         int[] result = new int[l];
