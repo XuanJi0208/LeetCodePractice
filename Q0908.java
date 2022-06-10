09e690c3eecb03ce6ab0619ad07ec13615724ddb
yaoxin <yaoxin.zhang@ucdconnect.ie>
2022-06-10 18:46:55 +0800

    Q0908 - 最小差值I

--- Q0908.java
+++ Q0908.java
@@ -1,5 +1,10 @@
 public class Q0908 {
     public int smallestRangeI(int[] nums, int k) {
+        //给你一个整数数组 nums，和一个整数 k 。
+        //在一个操作中，您可以选择 0 <= i < nums.length 的任何索引 i 。将 nums[i] 改为 nums[i] + x ，其中 x 是一个范围为 [-k, k] 的整数。对于每个索引 i ，最多 只能 应用 一次 此操作。
+        //nums的分数是nums中最大和最小元素的差值。
+        //在对nums 中的每个索引最多应用一次上述操作后，返回nums 的最低 分数 。
+
 //思路是找到最大数和最小数，用最大数减掉k，最小数+k，然后求差，若差＜=0，则返回0，反之，返回其本身。
         int l = nums.length;
         int min = nums[0], max = nums[0];
