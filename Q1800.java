2f522a02b61a5b071ade3df4504295d1bf51fae9
yaoxin <yaoxin.zhang@ucdconnect.ie>
2022-06-10 18:50:02 +0800

    Q1800 - 最大升序子数组和

--- Q1800.java
+++ Q1800.java
@@ -1,4 +1,8 @@
 public class Q1800 {
+    //给你一个正整数组成的数组 nums ，返回 nums 中一个 升序 子数组的最大可能元素和。
+    //子数组是数组中的一个连续数字序列。
+    //已知子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，若对所有 i（l <= i < r），numsi < numsi+1 都成立，则称这一子数组为 升序 子数组。注意，大小为 1 的子数组也视作 升序 子数组。
+
     public int maxAscendingSum(int[] nums) {
         //先设立一个max，逐个找出升序子数组，然后比较后存入max；
         int l = nums.length;
