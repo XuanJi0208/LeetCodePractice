a0af92cdfac5d9f2610e5d6fc3ff2afc240fc062
yaoxin <yaoxin.zhang@ucdconnect.ie>
2022-06-18 22:16:23 +0800

    剑指0017 - 打印从1到最大的n位数

--- R0017.java
+++ R0017.java
@@ -4,12 +4,9 @@
 
 public class R0017 {
     public int[] printNumbers(int n) {
-        int max = 1;
-        for (int i = 0; i < n; i++) {
-            max = max * 10;
-        }
-        int[] list = new int[max - 1];
-        for (int j = 1; j < max; j++) {
+        int max = (int) (Math.pow(10,n) -1);
+        int[] list = new int[max];
+        for (int j = 1; j <= max; j++) {
             list[j - 1] = j;
         }
         return list;
