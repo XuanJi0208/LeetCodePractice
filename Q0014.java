5bfd77a52c8b1e9df043a9040c22cdffb596cfd1
yaoxin <yaoxin.zhang@ucdconnect.ie>
2022-06-23 21:47:26 +0800

    Q0014 - 最长公共前缀

--- Q0014.java
+++ Q0014.java
@@ -2,7 +2,8 @@
  * @author XuanJi
  * 编写一个函数来查找字符串数组中的最长公共前缀。
  * 如果不存在公共前缀，返回空字符串 ""。
- * 看答案发现一个很巧妙的优化，即不需要记录实际的字符串，而只需要记录字符串有多长即可
+ * 看答案发现一个优化
+ * 即在我的catchTwo中，不需要记录实际的字符串，而只需要记录字符串有多长，再返回子串即可
  */
 public class Q0014 {
     public String longestCommonPrefix(String[] strs) {
