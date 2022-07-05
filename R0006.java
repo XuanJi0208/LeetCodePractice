cb2381a50c040b8f9d15c9d3664a16fc5e4c57d8
yaoxin <yaoxin.zhang@ucdconnect.ie>
2022-07-05 21:30:14 +0800

    R0006 - 从尾到头打印链表

--- R0006.java
+++ R0006.java
@@ -4,7 +4,7 @@ import java.util.Stack;
  * @author XuanJi
  * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
  */
-public class R006 {
+public class R0006 {
     public int[] reversePrint(ListNode head) {
         int[] res = new int[0];
         Stack stk = new Stack();
